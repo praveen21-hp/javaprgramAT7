69ee42ef88dcb7994e6b1eb3935a5026e97e2cd1
praveen21-hp <hppraveen3@gmail.com>
2024-07-09 23:30:33 +0530

    ALl file are   added

---
+++ src/ex_09072024/LAB002.java
@@ -0,0 +1,9 @@
+package ex_09072024;
+
+import javax.management.MBeanAttributeInfo;
+
+public class LAB002 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
