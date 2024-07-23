39dc0ca5f31a212a7f9f7d243b5d0c6c169eee9b
praveen21-hp <hppraveen3@gmail.com>
2024-07-23 23:12:30 +0530

    added files are  all added

---
+++ src/ex_09072024/lab012.java
@@ -0,0 +1,12 @@
+package ex_09072024;
+
+public class lab012
+{
+    public static void main(String[] args) {
+    public static void main(String[] args) {
+        // can i run class without main method?
+        // run option
+
+    }
+
+}
---
+++ src/ex_09072024/lab013.java
@@ -0,0 +1,8 @@
+package ex_09072024;
+
+public class lab013 {
+    public static void main(String args[]) {
+        System.out.println("yes");
+// can i have main method JVM will ignore all the remain main
+    }
+}
